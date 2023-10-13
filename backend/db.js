039017ff4b11029a6fceddc1ddc92fa0bb91e472
CharanaManawathilake <124888275+CharanaManawathilake@users.noreply.github.com>
2023-10-13 10:21:46 +0530

    "Backend test 2"

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "process.env.DB_KEY",
+  password: "qqqqq",
+  // password: "process.env.DB_KEY",
   database: "hrms",
 });
---
+++ backend/sql/user_test.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+  `user` (`user_name`, `password`, `permission_level_id`)
+VALUES
+  ('user1', 'password1', 'PERM01'),
+  ('user2', 'password2', 'PERM02'),
+  ('user3', 'password3', 'PERM03'),
+  ('user4', 'password4', 'PERM04'),
+  ('user5', 'password5', 'PERM05');
