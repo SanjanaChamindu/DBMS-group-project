0ef2af7157c29ff2d31519b0411d5ee362078cc4
ChaminduSanjana <chamindusannana@gmail.com>
2023-10-24 08:55:35 +0530

    changed

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "2^2+0.2ismyGPA",
+  password: "",
   // password: "process.env.DB_KEY",
   database: "hrms",
 });
