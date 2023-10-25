eab45ad0652a7581a77effa5c937e07296a1b3f8
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2023-10-25 23:59:05 +0530

    .

--- frontend/src/components/common/form.jsx
+++ frontend/src/components/common/form.jsx
@@ -131,3 +131,24 @@ class Form extends Component {
 }
 
 export default Form;
+
+const Input = ({name,label,error,...rest}) => {
+
+    //object destructuring
+    // component interface for input fields
+    return (
+        <div className="input-box">
+            <input
+                {...rest}       // rest operator, rest of the properties (type={type}, value={value}, onChange={onChange}})
+                name={name}
+                id={name}
+                placeholder={label}
+                autoFocus       //can type in the input field without clicking on it
+                autoComplete="off"
+                className="input-field"
+            />
+
+            {error && <div className="alert alert-danger">{error}</div>}        {/*if error is truthy, the div will be rendered*/}
+        </div>
+    );
+}
