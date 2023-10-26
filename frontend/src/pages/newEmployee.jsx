dd1639275949c3762cdf74cfe6549b461a0bda8c
Tishan Kashmeera <tishan.21@cse.mrt.ac.lk>
2023-10-26 16:26:52 +0530

    newEmployee

--- frontend/src/pages/newEmployee.jsx
+++ frontend/src/pages/newEmployee.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 export default function ProfilePage() {
   const [isEditing, setIsEditing] = useState(true);
-
+  
+  const [pageNumber, setPageNumber] = useState(1);
   // Basic Details
   const [fullName, setFullName] = useState('');
   const [nic, setNIC] = useState('');
@@ -16,53 +17,61 @@ export default function ProfilePage() {
   const [department, setDepartment] = useState('');
   const [employmentStatus, setEmploymentStatus] = useState('');
   const [nationality, setNationality] = useState('');
+  const [password, setPassword] = useState('');
 
-  // Work Details
-  const [personalAddress, setPersonalAddress] = useState('');
-  const [primaryPhoneNumber, setPrimaryPhoneNumber] = useState('');
-  const [secondaryPhoneNumber, setSecondaryPhoneNumber] = useState('');
-  const [emailAddress, setEmailAddress] = useState('');
-  const [emergencyContact1, setEmergencyContact1] = useState({ name: '', phoneNumber: '', relation: '' });
-  const [emergencyContact2, setEmergencyContact2] = useState({ name: '', phoneNumber: '', relation: '' });
-
-  // Define profilePicture and handleProfilePictureChange
-  const [profilePicture, setProfilePicture] = useState(
-    'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp'
-  );
 
   // Custom Fields
   const [addedCustomFields, setAddedCustomFields] = useState([]);
 
+    // Temporary array to store the data
+    const [temporaryData, setTemporaryData] = useState({
+        fullName: '',
+        nic: '',
+        gender: '',
+        birthday: '',
+        maritalStatus: '',
+        employeeID: '',
+        userName: '',
+        jobTitle: '',
+        supervisorID: '',
+        department: '',
+        employmentStatus: '',
+        nationality: '',
+        password: '',
+        customFields: [],
+      });
 
 
-  // Iterate and render custom fields in the Basic Details section
-    const renderCustomFields = () => {
-      return addedCustomFields.map((field, index) => (
-        <div key={index} className="row" style={{ marginBottom: '10px' }}>
-          <div className="col-md-4">
-            <p className="font-weight-bold">{field.label}:</p>
-          </div>
-          <div className="col-md-8">
-            {isEditing ? (
-              <div className="d-flex">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={field.value}
-                  onChange={(e) => {
-                    const updatedFields = [...addedCustomFields];
-                    updatedFields[index].value = e.target.value;
-                    setAddedCustomFields(updatedFields);
-                  }}
-                />
-              </div>
-            ) : (
-              <p>{field.value}</p>
-            )}
-          </div>
+
+// Iterate and render custom fields in the Basic Details section
+  const renderCustomFields = () => {
+    return addedCustomFields.map((field, index) => (
+      <div key={index} className="row" style={{ marginBottom: '10px' }}>
+        <div className="col-md-4">
+          <p className="font-weight-bold">{field.label}:</p>
         </div>
-      ));
-    };
+        <div className="col-md-8">
+          {isEditing ? (
+            <div className="d-flex">
+              <input
+                type="text"
+                className="form-control"
+                value={field.value}
+                onChange={(e) => {
+                  const updatedFields = [...addedCustomFields];
+                  updatedFields[index].value = e.target.value;
+                  setAddedCustomFields(updatedFields);
+                }}
+              />
+            </div>
+          ) : (
+            <p>{field.value}</p>
+          )}
+        </div>
+      </div>
+    ));
+  };
+
 
   useEffect(() => {
     // Simulate an asynchronous API call with setTimeout
@@ -80,17 +89,11 @@ export default function ProfilePage() {
         department: '',
         employmentStatus: '',
         nationality: '',
-        personalAddress: '',
-        primaryPhoneNumber: '',
-        secondaryPhoneNumber: '',
-        emailAddress: '',
-        emergencyContact1: { name: '', phoneNumber: '', relation: '' },
-        emergencyContact2: { name: '', phoneNumber: '', relation: '' },
         addedCustomFields: [
           { label: 'Custom Field 1', value: '' },
-          { label: 'Custom Field 2', value: '' },
           // Add more custom fields as needed
         ],
+        password: '',
         // profilePicture: 'URL_TO_YOUR_PROFILE_PICTURE',
       };
 
@@ -106,13 +109,9 @@ export default function ProfilePage() {
       setDepartment(dataFromBackend.department);
       setEmploymentStatus(dataFromBackend.employmentStatus);
       setNationality(dataFromBackend.nationality);
-      setPersonalAddress(dataFromBackend.personalAddress);
-      setPrimaryPhoneNumber(dataFromBackend.primaryPhoneNumber);
-      setSecondaryPhoneNumber(dataFromBackend.secondaryPhoneNumber);
-      setEmailAddress(dataFromBackend.emailAddress);
-      setEmergencyContact1(dataFromBackend.emergencyContact1);
-      setEmergencyContact2(dataFromBackend.emergencyContact2);
       setAddedCustomFields(dataFromBackend.addedCustomFields);
+      setPassword(dataFromBackend.password);
+      setPageNumber(1)
       // You can also set the profilePicture URL here
       // setProfilePicture(dataFromBackend.profilePicture);
     }, 1000); // Simulate a 1-second delay for the API call
@@ -125,6 +124,22 @@ export default function ProfilePage() {
   const handleSaveClick = () => {
     setIsEditing(false);
 
+    setTemporaryData({
+      fullName,
+      nic,
+      gender,
+      birthday,
+      maritalStatus,
+      employeeID,
+      userName,
+      jobTitle,
+      supervisorID,
+      department,
+      employmentStatus,
+      nationality,
+      password,
+      customFields: addedCustomFields,
+    });
   };
 
   const handleNextClick = () => {
@@ -132,21 +147,64 @@ export default function ProfilePage() {
       // If isEditing is true, display a message
       alert('Still editing');
     } else {
-      // If isEditing is false, proceed to the link
-      window.location.href = '/dashboard/employee-details/view-all-employees';
+      setPageNumber(2);
+      setIsEditing(true);
     }
   };
+
+
+  const handleBackClick = () => {
+    if (isEditing) {
+      // If isEditing is true, display a message
+      alert('Still editing');
+    } else {
+      setPageNumber(1);
+      setIsEditing(true);
+    }
+  };
+
+// Function to send data to the backend
+const handleDoneClick = () => {
+    if (isEditing) {
+      alert('Still editing');
+    } else {
+      // Check if all required fields are filled
+      if (
+        fullName.trim() === '' ||
+        nic.trim() === '' ||
+        gender.trim() === '' ||
+        birthday.trim() === '' ||
+        maritalStatus.trim() === '' ||
+        employeeID.trim() === '' ||
+        userName.trim() === '' ||
+        jobTitle.trim() === '' ||
+        supervisorID.trim() === '' ||
+        department.trim() === '' ||
+        employmentStatus.trim() === '' ||
+        nationality.trim() === '' ||
+        password.trim() === ''
+      ) {
+        alert('Please fill all the fields.');
+      } else {
+        // All required fields are filled, proceed with employee creation
+        // Send temporaryData to the backend
+        // Example API call:
+        alert('Employee created');
+      }
+    }
+  };
+  
 
 
 
 
   return (
-
-
+<div>
+    {pageNumber === 1 && (
           <div className="col-md-8 d-flex justify-content-end" style={{ marginTop: '10px' }}>
             <div className="card mb-4">
-              <div className="card-body">
-                <h2 className="mb-4">New Employee Details Page 1</h2>
+              <div className="card-body" style={{ width: '800px' }}>
+
 
                 {/* Basic Details Section */}
                 <div className="card mb-4">
@@ -163,6 +221,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={fullName}
                             onChange={(e) => setFullName(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{fullName}</p>
@@ -180,6 +239,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={nic}
                             onChange={(e) => setNIC(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{nic}</p>
@@ -197,6 +257,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={gender}
                             onChange={(e) => setGender(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{gender}</p>
@@ -214,6 +275,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={birthday}
                             onChange={(e) => setBirthday(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{birthday}</p>
@@ -231,6 +293,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={maritalStatus}
                             onChange={(e) => setMaritalStatus(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{maritalStatus}</p>
@@ -248,26 +311,10 @@ export default function ProfilePage() {
                             className="form-control"
                             value={employeeID}
                             onChange={(e) => setEmployeeID(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{employeeID}</p>
-                        )}
-                      </div>
-                    </div>
-                    <div className="row" style={{ marginBottom: '10px' }}>
-                      <div className="col-md-4">
-                        <p className="font-weight-bold">User Name:</p>
-                      </div>
-                      <div className="col-md-8">
-                        {isEditing ? (
-                          <input
-                            type="text"
-                            className="form-control"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                          />
-                        ) : (
-                          <p>{userName}</p>
                         )}
                       </div>
                     </div>
@@ -282,6 +329,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={jobTitle}
                             onChange={(e) => setJobTitle(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{jobTitle}</p>
@@ -299,6 +347,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={supervisorID}
                             onChange={(e) => setSupervisorID(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{supervisorID}</p>
@@ -316,6 +365,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={department}
                             onChange={(e) => setDepartment(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{department}</p>
@@ -333,6 +383,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={employmentStatus}
                             onChange={(e) => setEmploymentStatus(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{employmentStatus}</p>
@@ -350,6 +401,7 @@ export default function ProfilePage() {
                             className="form-control"
                             value={nationality}
                             onChange={(e) => setNationality(e.target.value)}
+                            required
                           />
                         ) : (
                           <p>{nationality}</p>
@@ -387,6 +439,88 @@ export default function ProfilePage() {
 
 </div>
 </div>
+  )}
+{pageNumber === 2 && (
 
+        <div className="col-md-8 d-flex justify-content-end" style={{ marginTop: '10px' }}>
+        <div className="card mb-4">
+          <div className="card-body" style={{width: '800px' }}>
+
+            {/* Basic Details Section */}
+            <div className="card mb-4">
+              <div className="card-body">
+                <h3 className="card-title" style={{ marginBottom: '10px' }}>User Name & Password</h3>
+                <div className="row" style={{ marginBottom: '10px' }}>
+                  <div className="col-md-4">
+                    <p className="font-weight-bold">User Name:</p>
+                  </div>
+                  <div className="col-md-8">
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                      />
+                    ) : (
+                      <p>{userName}</p>
+                    )}
+                  </div>
+                </div>
+                <div className="row" style={{ marginBottom: '10px' }}>
+                  <div className="col-md-4">
+                    <p className="font-weight-bold">Password:</p>
+                  </div>
+                  <div className="col-md-8">
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    ) : (
+                      <p>{password}</p>
+                    )}
+                  </div>
+                </div>
+                
+          </div>
+        </div>
+        </div>
+
+    {/* Edit Details Button */}
+    <div className="row">
+    <div className="col-lg-4">
+<div className="d-flex justify-content-left mb-2" style={{ marginLeft: '15px' }}>
+<button className="btn btn-primary" style={{ width: '150px', height: '40px', marginRight: '15px' }} onClick={handleBackClick}>Back</button>
+</div>
+</div>
+<div className="col-lg-4">
+<div className="d-flex justify-content-center mb-2" style={{ marginLeft: '15px' }}>
+  {isEditing ? (
+    <button className="btn btn-primary" style={{ width: '150px', height: '40px' }} onClick={handleSaveClick}>
+      Save
+    </button>
+  ) : (
+    <button className="btn btn-primary" style={{ width: '150px', height: '40px' }} onClick={handleEditClick}>
+      Edit Details
+    </button>
+  )}
+</div>
+</div>
+<div className="col-lg-4 d-flex justify-content-end">
+<button className="btn btn-primary" style={{ width: '150px', height: '40px', marginRight: '15px' }} onClick={handleDoneClick}>Done</button>
+</div>
+</div>
+
+
+</div>
+</div>
+
+)}
+</div>
   );
 }
