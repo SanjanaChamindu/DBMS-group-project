e3f9f34a1e4fc0ead61daca097d45606e50d0175
CharanaManawathilake <124888275+CharanaManawathilake@users.noreply.github.com>
2023-11-01 21:40:02 +0530

    ukjnyhbgtvfrcgthbyjnukjnyhbtgvrf

--- backend/controllers/custom.js
+++ backend/controllers/custom.js
@@ -10,7 +10,7 @@ export const viewCustomFields = (req, res) => {
         const permission_level_id = userInfo.permission_level_id;
 
         if (permission_level_id > '8193600002'){
-            const q1 = "SELECT attribute_name FROM custum_attributes"
+            const q1 = "SELECT attribute_name FROM custom_attributes"
             db.query(q1, (err, data1) => {
                 if (err) return res.status(500).json(err);
                 return res.json(data1);
@@ -49,6 +49,31 @@ export const viewCustomFieldsEmployees = (req, res) => {
 });
 }
 
+// export const addCustomFields = (req, res) => {
+//     const token = req.cookies.access_token;
+//     if (!token) return res.status(401).json("Not authenticated!");
+//     jwt.verify(token, "secretkey", (err, userInfo) => {
+//         if (err) return res.status(403).json("Token is not valid!");
+
+//         const permission_level_id = userInfo.permission_level_id;
+
+//         if (permission_level_id > '8193600002'){
+//             const q1 = "INSERT INTO custum_attributes(attribute_name) VALUES (?)"
+//             const values = [req.body.attribute_name];
+//             db.query(q1, [values], (err, data1) => {
+//                 if (err) return res.status(500).json(err);
+//                 const q2 = "ALTER TABLE employee ADD " + req.body.attribute_name + " VARCHAR(255)";
+//                 db.query(q2, (err, data2) => {
+//                     if (err) return res.status(500).json(err);
+//                     return res.json("Attribute added successfully!");
+//                 });
+//             });
+//         } else{
+//             return res.status(403).json("You are not authorized to view this data!");
+//         }
+// });
+// }
+
 export const addCustomFields = (req, res) => {
     const token = req.cookies.access_token;
     if (!token) return res.status(401).json("Not authenticated!");
@@ -58,7 +83,7 @@ export const addCustomFields = (req, res) => {
         const permission_level_id = userInfo.permission_level_id;
 
         if (permission_level_id > '8193600002'){
-            const q1 = "INSERT INTO custum_attributes(attribute_name) VALUES (?)"
+            const q1 = "INSERT INTO custom_attributes(attribute_name) VALUES (?)"
             const values = [req.body.attribute_name];
             db.query(q1, [values], (err, data1) => {
                 if (err) return res.status(500).json(err);
@@ -66,6 +91,31 @@ export const addCustomFields = (req, res) => {
                 db.query(q2, (err, data2) => {
                     if (err) return res.status(500).json(err);
                     return res.json("Attribute added successfully!");
+                });
+            });
+        } else{
+            return res.status(403).json("You are not authorized to view this data!");
+        }
+});
+}
+
+export const deleteCustomFields = (req, res) => {
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated!");
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!");
+
+        const permission_level_id = userInfo.permission_level_id;
+        const attribute_name = req.params.id;
+
+        if (permission_level_id > '8193600002'){
+            const q1 = "DELETE FROM custom_attributes WHERE attribute_name = ?"
+            db.query(q1, [attribute_name], (err, data1) => {
+                if (err) return res.status(500).json(err);
+                const q2 = "ALTER TABLE employee DROP " + attribute_name;
+                db.query(q2, (err, data2) => {
+                    if (err) return res.status(500).json(err);
+                    return res.json("Attribute deleted successfully!");
                 });
             });
         } else{
