8f4f0482e0a45d63ebf2917a33993ac9a95ae030
CharanaManawathilake <124888275+CharanaManawathilake@users.noreply.github.com>
2024-02-08 20:14:26 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "qqqqq",
-  // password: "process.env.DB_KEY",
+  // password: "",
+  password: "process.env.DB_KEY",
   database: "hrms",
 });
